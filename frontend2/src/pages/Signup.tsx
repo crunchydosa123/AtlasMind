6a1d2e47cd6d787d7965ba94c949e97f3724a5c6
Pratham Gadkari <prathamgadkari@gmail.com>
2025-10-27 23:18:34 +0530

    as

--- frontend2/src/pages/Signup.tsx
+++ frontend2/src/pages/Signup.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from '@/components/custom/SignupForm'
 
 const Signup = () => {
   return (
-    <div className='w-full h-screen flex justify-center content-center items-center bg-gray-300'>
+    <div className='w-full h-screen flex justify-center content-center items-center bg-gray-400'>
           <SignupForm />
         </div>
   )
